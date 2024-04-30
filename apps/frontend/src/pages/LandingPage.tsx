c2d34644c3c3e598f7d22a12de2d1f23b8bc535d
Tes-program <teslimodumuyiwa@gmail.com>
2024-04-30 07:42:12 +0100

    [FEAT]: Landing Page

--- apps/frontend/src/pages/LandingPage.tsx
+++ apps/frontend/src/pages/LandingPage.tsx
@@ -14,10 +14,10 @@ const LandingPage: React.FC = () => {
           <img src={line} className="absolute bottom-4 top-11" alt="Line" />
           <img src={circle} className="" alt="Circle" />
         </div>
-        <div className='pt-10 flex flex-col w-full border-x-[0.75px] border-gradient mx-2'>
+        <div className='pt-10 flex flex-col w-full border-x-[0.75px] border-gradient bg-gradient-to-br from-[rgba(0,0,0,0.052)] via-[rgba(209,25,25,0.05)] to-[rgba(166,174,187,0)] mx-2'>
         <h1 className="text-white font-monument-extended text-[16px] text-center">Welcome to the Certifcate Generator</h1>
         <div className='px-10 py-6'>
-            <p className='text-white text-center text-sm leading-snug font-normal'>
+            <p className='text-white text-center text-base leading-snug font-normal'>
             Easily generate your certificates and share them at your convenience. Anywhere, anytime and anyday 
             </p>
         </div>
