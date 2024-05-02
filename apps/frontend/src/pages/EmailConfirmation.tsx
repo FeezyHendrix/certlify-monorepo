4078e03d726aa42491d5e68d18f37f92092cce42
Tes-program <teslimodumuyiwa@gmail.com>
2024-05-02 19:21:10 +0100

    [FEAT]: First Phase Completed

--- apps/frontend/src/pages/EmailConfirmation.tsx
+++ apps/frontend/src/pages/EmailConfirmation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-// import abstract from "../assets/emailabstract.svg";
+import abstract from "../assets/emailabstract.svg";
 import curve from "../assets/curve.svg";
+import logo from "../assets/textlogo.svg";
 
 const EmailConfirmation: React.FC = () => {
     return (
@@ -8,8 +9,19 @@ const EmailConfirmation: React.FC = () => {
              <div className="hidden lg:block h-1/2">
         <img src={curve} className="w-60" alt="" />
       </div>
+      <div className="flex items-center justify-center flex-col my-32 lg:-mt-28 md:my-24 px-6 gap-3 ">
+        <div className="pb-10">
+        <img src={logo} className="" alt="Certlify" />
         </div>
-    )
+        <img src={abstract} alt="Abstract Illustration" />
+        <div className="md:w-1/4">
+        <p className="text-[#5D6B82] text-sm text-center">
+        A link has been sent to the email associated with your account please proceed to check your mail
+        </p>
+        </div>
+      </div>
+        </div>
+    );
 }
 
 export default EmailConfirmation;
